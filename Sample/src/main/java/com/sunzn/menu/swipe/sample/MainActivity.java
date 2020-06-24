85b533fdcb98e04050c3b8c0c3a539c0d5173435
sunzn <sunzn.cn@gmail.com>
2020-06-24 17:25:27 +0800

    Update Library

--- Sample/src/main/java/com/sunzn/menu/swipe/sample/MainActivity.java
+++ Sample/src/main/java/com/sunzn/menu/swipe/sample/MainActivity.java
@@ -1,11 +1,12 @@
 package com.sunzn.menu.swipe.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.MotionEvent;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sunzn.menu.swipe.library.OnActivityTouchListener;
 import com.sunzn.menu.swipe.library.RecyclerTouchListener;
